ef6d17e7af14dc1075575ab7f4b317efa6b1ed2e
Jacob Milham <wpsrobotics@hotmail.com>
2024-02-16 16:18:53 -0500

    Added launcher and started arm code

---
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -0,0 +1,91 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkBase.ControlType;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.SubsystemConstants;
+
+public class ClimbSubsystem extends SubsystemBase {
+  private final CANSparkMax m_climbMotor1 = new CANSparkMax(SubsystemConstants.kClimbMotor1CanId, MotorType.kBrushed);
+  private final CANSparkMax m_climbMotor2 = new CANSparkMax(SubsystemConstants.kClimbMotor2CanId, MotorType.kBrushed);
+
+  private SparkPIDController m_climbPid;
+  private RelativeEncoder m_climbEncoder;
+
+  private static double kClimbP = 0.01;
+  private static double kClimbI = 0;
+  private static double kClimbD = 0;
+  private static double kClimbFF = 0;
+
+  /** Creates a new ClimbSubsystem. */
+  public ClimbSubsystem() {
+    m_climbMotor1.restoreFactoryDefaults();
+    m_climbMotor2.restoreFactoryDefaults();
+
+    m_climbMotor1.setIdleMode(IdleMode.kBrake);
+    m_climbMotor2.setIdleMode(IdleMode.kBrake);
+
+    m_climbMotor2.follow(m_climbMotor1);
+
+    m_climbPid = m_climbMotor1.getPIDController();
+
+    m_climbPid.setP(kClimbP);
+    m_climbPid.setI(kClimbI);
+    m_climbPid.setD(kClimbD);
+    m_climbPid.setFF(kClimbFF);
+
+    m_climbEncoder = m_climbMotor1.getEncoder();
+
+    smartDashboardInit();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    smartDashboardUpdate();
+  }
+
+  public void smartDashboardInit()
+  {
+      SmartDashboard.putNumber("Climb Position", m_climbEncoder.getPosition());
+  
+      SmartDashboard.putBoolean("Go To Climb Target", false);
+
+      SmartDashboard.putBoolean("Climb Data Update", false);
+      SmartDashboard.putNumber("Climb P", kClimbP);
+      SmartDashboard.putNumber("Climb I", kClimbI);
+  }
+
+  private void smartDashboardUpdate()
+  {
+      SmartDashboard.putNumber("Climb Position", m_climbEncoder.getPosition());
+
+      if (SmartDashboard.getBoolean("Climb Data Update", false))
+      {
+        m_climbPid.setP(SmartDashboard.getNumber("Climb P", kClimbP));
+        
+        SmartDashboard.putBoolean("Climb Data Update", false);
+      }
+
+      if (SmartDashboard.getBoolean("\"Go To Climb Target", false))
+      {
+        goToPosition(SmartDashboard.getNumber("Climb Target Position", 0));
+        
+        SmartDashboard.putBoolean("\"Go To Climb Target", false);
+      }
+  }
+
+  public void goToPosition(double position) {
+    m_climbPid.setReference(position, ControlType.kPosition);
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.SubsystemConstants;
+
+public class ShooterSubsystem extends SubsystemBase {
+  private final CANSparkMax m_leftShooter = new CANSparkMax(SubsystemConstants.kLeftShooterCanId, MotorType.kBrushed);
+  private final CANSparkMax m_leftFeeder = new CANSparkMax(SubsystemConstants.kLeftFeederCanId, MotorType.kBrushed);
+  private final CANSparkMax m_rightShooter = new CANSparkMax(SubsystemConstants.kRightShooterCanId, MotorType.kBrushed);
+  private final CANSparkMax m_rightFeeder = new CANSparkMax(SubsystemConstants.kRightFeederCanId, MotorType.kBrushed);
+
+  /** Creates a new ShooterSubsystem. */
+  public ShooterSubsystem() {
+    m_leftShooter.restoreFactoryDefaults();
+    m_leftFeeder.restoreFactoryDefaults();
+    m_rightShooter.restoreFactoryDefaults();
+    m_rightFeeder.restoreFactoryDefaults();
+
+    m_leftShooter.setIdleMode(IdleMode.kCoast);
+    m_leftFeeder.setIdleMode(IdleMode.kCoast);
+    m_rightShooter.setIdleMode(IdleMode.kCoast);
+    m_rightFeeder.setIdleMode(IdleMode.kCoast);
+
+    m_rightShooter.follow(m_leftShooter);
+    m_rightShooter.setInverted(true);
+    m_rightFeeder.follow(m_leftFeeder);
+    m_rightFeeder.setInverted(true);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void setShooterSpeed(double speed) {
+    m_leftShooter.set(speed);
+  }
+  public void setFeederSpeed(double speed) {
+    m_leftFeeder.set(speed);
+  }
+}
